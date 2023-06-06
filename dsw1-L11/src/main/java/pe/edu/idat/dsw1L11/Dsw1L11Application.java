2ee3478b74c6c62796282888a6bb097e14894b9f
Alvg01 <AT74881537@idat.edu.pe>
2023-06-05 19:22:57 -0500

    Proyect Inicial 01

---
+++ dsw1-L11/src/main/java/pe/edu/idat/dsw1L11/Dsw1L11Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsw1L11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1L11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1L11Application.class, args);
+	}
+
+}
---
+++ dsw1-L11/src/test/java/pe/edu/idat/dsw1L11/Dsw1L11ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsw1L11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1L11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
